4252b5392cd123e5ac6de5d9dc90f1e1348ce2a8
Gavi <guicavila@gmail.com>
2023-07-26 19:04:52 -0300

    Moved functions to proper file and solved main problem with main game loop. To-do: Fix checkWin()

---
+++ functions.py
@@ -0,0 +1,39 @@
+import classes as c
+
+board = c.Board()
+
+def played(xy):
+	played = []
+	for coord in xy:
+		try:
+			played.append(int(coord))
+		except:
+			if coord == 'a':
+				played.append(1)
+			elif coord == 'b':
+				played.append(2)
+			elif coord == 'c':
+				played.append(3)
+			else:
+				return 'Error: invalid coord'
+	#print(played)
+	return played
+
+def counter(pC):
+    for index, value in enumerate(board.boardDict.values()):
+        if all(x == y for x, y in zip(value, pC)):
+        	print(index)
+        	return index
+    return 'Error: invalid cell'
+
+def play(p, xy):
+	if len(xy) != 2:
+		return 'Error: invalid coord'
+	pC = played(xy)
+	count = counter(pC)
+	if count == 'Error: invalid cell':
+		return 'Error: invalid cell'
+
+	board.boardDict.update({c.grob[f'c{count}']:p})
+	c.grob[f'c{count}'] = c.Cell(pC[0], pC[1], p)
+	return
--- main.py
+++ main.py
@@ -1,56 +1,24 @@
-import classes as c
-from time import sleep as slp
+import functions as f
 
-a = 'Error: invalid cell'
-board = c.Board()
-
-def play(p, xy):
-	played = []
-	for coord in xy:
-		try:
-			played.append(int(coord))
-		except:
-			if coord == 'a':
-				played.append(1)
-			elif coord == 'b':
-				played.append(2)
-			elif coord == 'c':
-				played.append(3)
-			else:
-				return 'Error: invalid coord'
-	count = 0
-	for cell in board.boardDict:
-		count += 1
-		if board.boardDict[cell] == played:
-			break
-	print(c.grob[f'c{count}'].state)
-	slp(1)
-	if c.grob[f'c{count}'].state == '-':
-		c.grob[f'c{count}'] = c.Cell(played[0], played[1], p)
-		return ''
-	else:
-		return a
-
-while board.win == '':
-	print(board.boardDict)
-	print(board)
+while f.board.win == '':
+	print(f.board.boardDict)
+	print(f.board)
 	pX = input("Player X's turn. Where do you want to play? (l#)\n")
-	play('X', pX)
-	while play('X', pX) != '':
-		print(a)
+	while f.play('X', pX) != None:
+		print(f.play('X', pX))
 		pX = input("Player X's turn. Where do you want to play? (l#)\n")
-	print(board.boardDict)
-	print(board)
-	if board.checkWin() != '':
-		print(board.checkWin())
+	print(f.board.boardDict)
+	print(f.board)
+	if f.board.checkWin() != '':
+		print(f.board.checkWin())
 		break
+
 	pO = input("Player O's turn. Where do you want to play? (l#)\n")
-	play('O', pO)
-	while play('O', pO) != '':
-		print(a)
+	while f.play('O', pO) != None:
+		print(f.play('X', pX))
 		pO = input("Player O's turn. Where do you want to play? (l#)\n")
-	print(board.boardDict)
-	print(board)
-	if board.checkWin() != '':
-		print(board.checkWin())
+	print(f.board.boardDict)
+	print(f.board)
+	if f.board.checkWin() != '':
+		print(f.board.checkWin())
 		break
